fc83afb39f333506673717cb7925dc3cc7577afb
Cu Test Inc <arvi.nick.90@gmail.com>
2023-12-29 18:02:41 +0400

    last commit

--- mainDrift.py
+++ mainDrift.py
@@ -49,19 +49,22 @@ def main():
         for row in data:
             result = [float(x.strip(' "')) for x in row]
             data_matrix = np.vstack((data_matrix, result))
-
+    second_ticks_labels = np.arange(0, int(result[0]), 30)
     globs.X, y_cent_original = readPitchTimeDomain.readPitchTimeDomain(data_matrix)
     print("Pitch File read and saved in datamatrix")
     fig, subplot = plt.subplots(figsize=(20, 10))
     y_cent_nan = np.copy(y_cent_original)
     y_cent_nan[y_cent_nan == 0] = np.nan
 
-    subplot.plot(y_cent_nan, linewidth=0.5, color="k")
+    subplot.plot(y_cent_nan, linewidth=0.5, color="k", label="frequency")
 
     if NOT_PAPER_PRINT:
         subplot.set_title("Time-Frequency")
-    subplot.set_xlabel('time')
+    subplot.set_xticks(np.linspace(0, y_cent_nan.size, second_ticks_labels.size),
+                       labels=second_ticks_labels)
+    subplot.set_xlabel('time (seconds)')
     subplot.set_ylabel('detected frequencies (cents)')
+    subplot.legend()
     fig.savefig("Time-Frequency.png")
     fig.show()
 
@@ -75,12 +78,13 @@ def main():
         histo_end -= 1
 
     fig, subplot = plt.subplots()
-    subplot.plot(histogram_smooth)
+    subplot.plot(histogram_smooth, color="b", label="smoothed histogram")
     subplot.set_xlim(histo_start - 10, histo_end + 10)
     if NOT_PAPER_PRINT:
         subplot.set_title("histogram")
     subplot.set_xlabel('detected frequencies (cents)')
     subplot.set_ylabel('number of occurances')
+    subplot.legend()
     fig.savefig("mainhisto.png")
     fig.show()
 
@@ -162,7 +166,7 @@ def main():
                                             ) + segment_bounderies["start"]
     for idx, row in segment_bounderies.iterrows():
         subplot.scatter([(row["tick_positions"]) for i in range(row["peaks"].size)],
-                        row["peaks"], color="b")
+                        row["peaks"], color="b", label="main peaks in each sentence" if idx == 0 else "")
     for m in np.arange(1 - 1, (locs_table.shape[0])):
         for k in np.arange(1 - 1, 10):
             if locs_table[m, k] != 0:
@@ -174,16 +178,19 @@ def main():
 
     # subplot.scatter(x_coordinates, y_coordinates / 10)
     subplot.set_xticks(segment_bounderies["tick_positions"].values.tolist(),
-                       labels=segment_bounderies.index.tolist())
+                       labels=list(segment_bounderies.index + 1),
+                       rotation=90,
+                       fontdict={"size": 6})
     min_y = np.nanmin(y_coordinates_v) / 10
     max_y = np.nanmax(y_coordinates_v) / 10
     if NOT_PAPER_PRINT:
         subplot.set_title("Detected frequencies in segments")
     subplot.set_xlabel('sentence number')
     subplot.set_ylabel('detected frequencies')
-    # subplot.set_xticks([x for x in range(seg.size)])
-    subplot.vlines(subplot.get_xticks(), 0, subplot.get_ylim()[1], linestyles="dashed", linewidth=0.2)
+    subplot.vlines(subplot.get_xticks(), 0, subplot.get_ylim()[1], linestyles="dashed", linewidth=0.2,
+                   label="sentence indicator")
     fig.savefig("frequency-segment.png")
+    subplot.legend()
     fig.show()
 
     matrix = np.zeros((1, 2))
@@ -226,33 +233,41 @@ def main():
     # #############################################################################
     # Plot result of classification
 
-    fig, subplots = plt.subplot_mosaic([["audio", "audio", "audio", "audio", "audio"],
-                                        ["cluster", "cluster", "cluster", "cluster", "cluster"],
-                                        ["cluster", "cluster", "cluster", "cluster", "cluster"],
-                                        ["cluster", "cluster", "cluster", "cluster", "cluster"],
-                                        ["cluster", "cluster", "cluster", "cluster", "cluster"]])
+    fig, subplots = plt.subplot_mosaic([["audio", "audio", "audio", "audio", "legend"],
+                                        ["cluster", "cluster", "cluster", "cluster", "legend"],
+                                        ["cluster", "cluster", "cluster", "cluster", "legend"],
+                                        ["cluster", "cluster", "cluster", "cluster", "legend"],
+                                        ["cluster", "cluster", "cluster", "cluster", "legend"]],
+                                       figsize=(20, 10))
     subplots["cluster"].sharex(subplots["audio"])
+    subplots["legend"].tick_params(bottom=False, labelbottom=False,
+                                   left=False, labelleft=False)
+    subplots["legend"].spines['left'].set_visible(False)
+    subplots["legend"].spines['right'].set_visible(False)
+    subplots["legend"].spines['bottom'].set_visible(False)
+    subplots["legend"].spines['top'].set_visible(False)
 
-    # Black removed and is used for noise instead.
     unique_labels = set(labels)
     colors_ = [choice(list(colors.TABLEAU_COLORS.values())) for i in np.linspace(0, 1, len(unique_labels))]
     xy_color = pd.DataFrame(columns=segment_bounderies["tick_positions"].astype(int),
                             index=np.concatenate([i.flatten() for i in segment_bounderies["peaks"]]))
+    label_colors = {}
+    label_legned = {-1: "noise",
+                    0: "first cluster",
+                    1: "second cluster"}
     for k, col in zip(unique_labels, colors_):
         if k == -1:
             # Black used for noise.
             col = [0, 0, 0, 1]
         class_member_mask = labels == k
-
+        label_colors[k] = col
         xy = matrix_v[class_member_mask]
         subplots["cluster"].plot(xy[:, 0], xy[:, 1] / 10, "o", markerfacecolor=col, markeredgecolor=col,
-                                 markersize=4)
+                                 markersize=3,
+                                 label=label_legned[k] if k == 0 or k == 1 or k == -1 else str(k + 1) + "th cluster")
         for row_number in range(xy.shape[0]):
             if isinstance(col, str):
-                xy_color.loc[int(xy[row_number][1]/10), int(xy[row_number][0])] = col
-        # xy = matrix_v[class_member_mask & ~core_samples_mask]
-        # subplots["cluster"].plot(xy[:, 0], xy[:, 1] / 10, "o", markerfacecolor=col, markeredgecolor=col,
-        #                          markersize=4)
+                xy_color.loc[int(xy[row_number][1] / 10), int(xy[row_number][0])] = col
 
     # xy_color.columns /= 10
     xy_color.replace('nan', np.nan, inplace=True)
@@ -261,7 +276,8 @@ def main():
     for p in range(n_clusters_):
         xrange = range(1 + int(samples_w_labels.max(0)[0]))
         yrange = mm[p] * xrange + bb[p]
-        subplots["cluster"].plot(xrange, yrange / 10)
+        subplots["cluster"].plot(xrange, yrange / 10, linestyle="dashed", color="k", linewidth=0.6,
+                                 label="clusters indicator lines" if p == 0 else "")
         subplots["cluster"].set_ylim([min_y - 100, max_y + 100])
     if NOT_PAPER_PRINT:
         subplots["cluster"].set_title(" number of clusters: %d" % n_clusters_)
@@ -269,9 +285,11 @@ def main():
     for idx, segment in np.ndenumerate(seg):
         start_point = segment.curveBeg
         end_point = segment.curveEnd
-        num = y_cent_extrapolated[segment.curveBeg: segment.curveEnd].size
+        audio_line = y_cent_extrapolated[segment.curveBeg: segment.curveEnd]
+        audio_line = np.where(audio_line == 0, np.nan, audio_line)
+        num = audio_line.size
         subplots["audio"].plot(np.linspace(start_point, end_point, num),
-                               y_cent_extrapolated[segment.curveBeg: segment.curveEnd],
+                               audio_line,
                                linewidth=0.2,
                                color="b" if idx[0] % 2 == 1 else "r")
 
@@ -289,15 +307,24 @@ def main():
                 colors_[int(color_y - min_y + 80): int(color_y - min_y + 120)] = [colors.to_rgb(color) for i in
                                                                                   range(40)]
 
-        subplots["cluster"].scatter(scatter_x, scatter_y, s=0.01, c=colors_)
+        subplots["cluster"].scatter(scatter_x, scatter_y, s=0.005, c=colors_)
 
     subplots["audio"].tick_params(axis="x", which='both', bottom=False, labelbottom=False)
     subplots["audio"].set_ylim(min_y - 500, max_y + 500)
     subplots["cluster"].set_xlabel('time (seconds)')
     subplots["cluster"].set_ylabel('detected frequencies')
-    subplots["cluster"].set_xticks(np.linspace(0, seg[-1].curveEnd, 5),
-                                   labels=np.linspace(0, int(result[0]), 5).astype(int))
-    fig.suptitle("placeholder")
+    subplots["cluster"].set_xticks(np.linspace(0, seg[-1].curveEnd, second_ticks_labels.size),
+                                   labels=second_ticks_labels)
+    subplots["audio"].legend()
+    subplots["cluster"].legend()
+    cluster_legend = subplots["cluster"].get_legend()
+    audio_legend = subplots["audio"].get_legend()
+    legend_handles = cluster_legend.legend_handles.copy()
+    legend_handles.extend(audio_legend.legend_handles)
+    legend_labels = [handle.get_label() for handle in legend_handles]
+    subplots["legend"].legend(handles=legend_handles, labels=legend_labels)
+    for name in ["audio", "cluster"]:
+        subplots[name].legend().remove()
     fig.show()
     fig.savefig("classification")
     print('end of prog')
